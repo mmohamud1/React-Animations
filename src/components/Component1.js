eaab70f2eb3f417143883ebf41952a16948d3dce
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-10-21 20:46:28 +0200

    Commit

---
+++ src/components/Component1.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Component1 = () => {
+    return (
+        <div style={c1Style}>
+            <h1>Component 1</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, expedita quas voluptates consequatur perspiciatis esse quis temporibus et corrupti, optio exercitationem non earum dolor pariatur id obcaecati cupiditate praesentium minima!</p>
+        </div>
+    )
+}
+
+const c1Style = {
+    background: 'steelblue',
+    color: 'white',
+    padding: '1.5rem'
+}
+
+export default Component1
