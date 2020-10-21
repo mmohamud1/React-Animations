42ee96a620c6eec3e97a918d65246d8ea08bcd2e
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-10-21 20:37:52 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
