2e40605d9de9f75c47a96d1af9183e5f89c38824
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-10-21 20:57:55 +0200

    Commit

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import {Spring} from 'react-spring/renderprops'
+import React, { Component } from 'react'
+import {Spring} from 'react-spring/renderprops' 
 
-
-const Component1 = () => {
-    return (
-        <Spring from={{ opacity: 0, marginTop: -500 }} to={{ opacity: 1, marginTop: 0 }}>
+export class Component2 extends Component {
+    render() {
+        return (
+            <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
             {props => (
                 <div style={props}>
-                    <div style={c1Style}>
-                        <h1>Component 1</h1>
+                    <div style={c2Style}>
+                        <h1>Component 2</h1>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, expedita quas voluptates consequatur perspiciatis esse quis temporibus et corrupti, optio exercitationem non earum dolor pariatur id obcaecati cupiditate praesentium minima!</p>
                     </div>
                 </div>
             )}
         </Spring>
-    )
+        )
+    }
 }
 
-const c1Style = {
-    background: 'steelblue',
+const c2Style = {
+    background: 'slateblue',
     color: 'white',
     padding: '1.5rem'
 }
 
-export default Component1
+export default Component2
