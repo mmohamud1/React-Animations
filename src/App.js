88695ef25f73d3076ee259481328507e3cb0a340
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-16 13:23:36 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Hero from './components/Hero';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Hero />
       </div>
     );
   }
