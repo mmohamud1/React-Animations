0e23ac629873cbfb6a19ab839989aca2285f9b41
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-16 16:40:03 +0000

    Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Image from '../img/bg-1.jpg'
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Image from '../img/bg-1.jpg';
 
 const Home = () => {
-    return (
-        <>
-            <Header />
-            <Hero image={Image} title=''/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <Hero image={Image} title='' />
+    </>
+  );
+};
 
-export default Home
+export default Home;
