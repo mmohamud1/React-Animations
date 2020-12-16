bbefcd3346c89c91a48fb406967084b5d817883d
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-16 14:08:11 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from './pages/About';
+import Services from './pages/Services';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route path='/' component={Home} />
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/services' component={Services} />
         </Switch>
       </Router>
     );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Image from '../img/bg-1.jpg';
+
+const Section = styled.section`
+  background: url(${Image}) center;
+  height: 100vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  margin-top: -60px;
+`;
+
+const Container = styled.div`
+  color: #fff;
+  padding: 2rem;
+  backdrop-filter: blur(5px);
+  background-color: rgba(0, 0, 0, 0.4);
+  margin: 2rem;
+  border-radius: 10px;
+
+  h1 {
+    font-size: clamp(2rem, 8vw, 5rem);
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    font-size: clamp(1rem, 6vw, 2.5rem);
+    margin-bottom: 1rem;
+  }
+`;
 
 const Hero = () => {
-    return (
-        <div>
-            Hero
-        </div>
-    )
-}
+  return (
+    <Section>
+      <Container>
+        <h1>title</h1>
+        <p>description</p>
+        <button>Learn more</button>
+      </Container>
+    </Section>
+  );
+};
 
-export default Hero
+export default Hero;
