1fcc63f64a2f99190e5be5368958e30b0a10640a
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-16 12:43:53 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Component1 from './components/Component1'
-import Component2 from './components/Component2'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Component1 />
-        <Component2 />
+        <h1>Hello World</h1>
       </div>
     );
   }
